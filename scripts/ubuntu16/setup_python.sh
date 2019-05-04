892df6fedd2e1b1264d68a519ecb128287342cd8
ziven <ziven@zerone.io>
2019-05-04 23:28:14 +0800

    Fixed script for setup python

--- scripts/ubuntu16/setup_python.sh
+++ scripts/ubuntu16/setup_python.sh
@@ -3,5 +3,8 @@
 
 # ----------------- Setup Python and Pip ------------------
 
-apt-get install -y python-pip
-pip install -U pip
+# apt-get install -y python-pip
+# pip install -U pip
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
